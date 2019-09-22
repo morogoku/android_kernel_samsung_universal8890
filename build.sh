6ca23fe1413683a3b23a35f17cd0bbbe6a2896b9
morogoku <morogoku@hotmail.com>
2019-09-22 22:35:46 +0200

    kernel version r04
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- build.sh
+++ build.sh
@@ -2,20 +2,30 @@
 #
 
 
-
-# SETUP
-# -----
 export ARCH=arm64
 export SUBARCH=arm64
 export CROSS_COMPILE=/home/moro/kernel/toolchains/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
+
 DEFCONFIG=exynos8890-hero2lte_defconfig
+
+if [ ! -z $1 ]; then
+    case $1 in 
+    flat)
+        DEFCONFIG=exynos8890-herolte_defconfig
+        ;;
+    *)
+        DEFCONFIG=exynos8890-hero2lte_defconfig
+        ;;
+    esac
+fi
+
 
 export ANDROID_MAJOR_VERSION=o 
 export ANDROID_VERSION=80000 
 export PLATFORM_VERSION=8.0.0
 
-export LOCALVERSION="twrp-r03"
+export LOCALVERSION="twrp-r04"
 
 (
 
